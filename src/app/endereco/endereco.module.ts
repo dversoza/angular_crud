f026cf8d16b2bc5f95134348c5c6e6bd1bc28305
Daniel Versoza Alves <daniel.ve3@gmail.com>
2021-08-28 12:55:00 -0300

    Finished directives and pipes exercices

--- src/app/endereco/endereco.module.ts
+++ src/app/endereco/endereco.module.ts
@@ -8,6 +8,8 @@ import { EnderecoService } from './services';
 import { ListarEnderecosComponent } from './listar-enderecos/listar-enderecos.component';
 import { InserirEnderecosComponent } from './inserir-enderecos/inserir-enderecos.component';
 import { EditarEnderecosComponent } from './editar-enderecos/editar-enderecos.component';
+import { SharedModule } from '../shared';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,13 @@ import { EditarEnderecosComponent } from './editar-enderecos/editar-enderecos.co
     InserirEnderecosComponent,
     EditarEnderecosComponent,
   ],
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    SharedModule,
+    NgxMaskModule.forRoot(),
+  ],
   providers: [EnderecoService],
 })
 export class EnderecoModule {}
